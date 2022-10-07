9376a143fa2d6b94728440963461e29a317ab575
Rahul-s-007 <70894220+Rahul-s-007@users.noreply.github.com>
2022-10-07 15:22:15 +0530

    Update driver.py

--- Score brisque/driver.py
+++ Score brisque/driver.py
@@ -2,12 +2,15 @@ from brisquemodule import BRISQUE
 import os
 
 def calscore(path):
+    slab = 45 # Maximum score after which image will be considered of bad quality
+    
     dir_list = os.listdir(path)
     for i in dir_list:
         if i.lower().endswith(('.png', '.jpg', '.jpeg')):
             img_path = path+"\\"+i
             obj = (BRISQUE(img_path, url=False)).score()
-            if(obj<45):
+            
+            if(obj < slab):
                 print("Good Image-",i,":",obj)
             else:
                 print("Bad Image-",i,":",obj)
